6a5bc1dcea15db33838cbf248f87cdee73821e52
Habib <habib@MacBook-Air.local>
2026-07-24 10:54:14 +0300

    Fix Deploy website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "494684118" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "625836099" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
